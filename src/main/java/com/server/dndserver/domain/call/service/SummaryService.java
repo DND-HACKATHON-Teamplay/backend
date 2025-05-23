b4e5a7623fe6cff85962ed90fa070f5856b9c026
JangIkhwan <bluensky0213@gmail.com>
2025-05-24 00:23:40 +0900

    [Feat] #16 요약 조회 API 구현

--- src/main/java/com/server/dndserver/domain/call/service/SummaryService.java
+++ src/main/java/com/server/dndserver/domain/call/service/SummaryService.java
@@ -3,17 +3,22 @@ package com.server.dndserver.domain.call.service;
 import com.server.dndserver.domain.call.domain.Call;
 import com.server.dndserver.domain.call.dto.SummaryDTO;
 import com.server.dndserver.domain.call.repository.CallRepository;
+import com.server.dndserver.domain.conversation.domain.Conversation;
+import com.server.dndserver.domain.conversation.repository.ConversationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SummaryService {
     private final CallRepository callRepository;
+    private final ConversationRepository conversationRepository;
     public SummaryDTO getSummary(Long callId) {
         Call call = callRepository.findById(callId).orElseThrow(() -> new IllegalArgumentException("전화 조회 실패"));
-
-        return SummaryDTO.createFromCall(call);
+        List<Conversation> conversations = conversationRepository.findByCallId(call.getId());
+        return SummaryDTO.createFromCallAndConversationList(call, conversations);
 
     }
 }
