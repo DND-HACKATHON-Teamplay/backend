017d1a43f2eb1439098dd3236eb332686a81b93d
Minu Kim <kmw106933@naver.com>
2025-05-24 03:29:43 +0900

    [feat] 어르신 API 추가 (#28)
    * [feat] 대화 등록 API 추가
    
    * [feat] 날마다의 전화 API

--- src/main/java/com/server/dndserver/domain/call/controller/CallController.java
+++ src/main/java/com/server/dndserver/domain/call/controller/CallController.java
@@ -1,6 +1,7 @@
 package com.server.dndserver.domain.call.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.server.dndserver.domain.call.domain.Call;
 import com.server.dndserver.domain.call.dto.CallRequestDTO;
 import com.server.dndserver.domain.call.dto.CallResponseDTO;
 import com.server.dndserver.domain.call.service.CallService;
@@ -9,6 +10,7 @@ import com.server.dndserver.global.annotation.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,14 +35,24 @@ public class CallController {
         return callService.getMonthlyEvaluationByMember(member.getId(), year, month);
     }
 
-   @PostMapping
-   @Tag(name = "전화 API")
-   @Operation(summary = "어르신의 전화 내용을 기록합니다.")
+    @PostMapping
+    @Tag(name = "전화 API")
+    @Operation(summary = "어르신의 전화 내용을 기록합니다.")
     public ResponseEntity<CallResponseDTO> createCall(
             @RequestBody CallRequestDTO content) throws JsonProcessingException {
 
         return ResponseEntity.ok(callService.saveCall(content));
     }
 
+    @GetMapping("/daily")
+    @Tag(name = "전화 API")
+    @Operation(summary = "어르신의 날마다의 상태를 반환합니다. JWT accessToken이 필요합니다.")
+    public Call getDailyCall(
+            @AuthUser Member member,
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
+        return callService.getDailyCall(member.getId(), date);
+    }
 
 }
