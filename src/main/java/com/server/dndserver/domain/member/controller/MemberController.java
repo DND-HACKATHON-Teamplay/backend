bf644715bfda2fe0c83279da1d569a602bcc7ad1
Minu Kim <kmw106933@naver.com>
2025-05-23 16:09:05 +0900

    [feat] 멤버 탈퇴 API

--- src/main/java/com/server/dndserver/domain/member/controller/MemberController.java
+++ src/main/java/com/server/dndserver/domain/member/controller/MemberController.java
@@ -4,6 +4,8 @@ import com.server.dndserver.domain.member.domain.Member;
 import com.server.dndserver.domain.member.service.LogoutService;
 import com.server.dndserver.domain.member.service.MemberService;
 import com.server.dndserver.global.annotation.AuthUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +19,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @DeleteMapping
+    @Tag(name = "유저 API")
+    @Operation(summary = "회원 탈퇴를 진행합니다. 요청 헤더에 포함된 JWT Access Token이 필요합니다.")
     public ResponseEntity<Void> deleteMember(@AuthUser Member member) {
         memberService.deleteMember(member);
         return ResponseEntity.ok().build();
